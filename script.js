4ddc9ae2152fdd9ea7fdcef716be56724a002ef2
marchyshyn <amintux@gmail.com>
2015-09-08 13:18:50 +0300

    ism10

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ function myFunction() {
   if (square != null) {
    var wrapperWidth = $('.wrapper').width();
    var wrapperHeight = $('.wrapper').height();
-   var elements = (wrapperWidth / squareSize |0) * (wrapperHeight / squareSize |0);
+   console.log(wrapperWidth + ' wrapperWidth');
+   console.log(wrapperHeight + ' wrapperHeight');
+   var elements = Math.floor(wrapperWidth / squareSize) * Math.floor(wrapperHeight / squareSize);
+   console.log(elements + ' elements');
+   console.log(Math.floor(wrapperWidth / squareSize));
+   console.log((wrapperWidth / squareSize));
+   console.log( Math.floor(wrapperHeight / squareSize));
+   console.log( (wrapperHeight / squareSize))
    var lineItems = wrapperWidth / squareSize |0;
-   var columnItems = wrapperHeight / squareSize |0;
+   var columnItems = wrapperHeight / squareSize |0,
+        $html = '',
+        $wrapper = $('.wrapper');
+
    for (var i = 0; i < elements; i++) {
-    $('.wrapper').append('<div class="child"><div>');
-    $('.child').css({
-     width: squareSize,
-     height: squareSize
-   });
+    $html += '<div class="child"></div>';   
   };
+
+  $wrapper.append($html);
+    var hue = 'rgb(' + (Math.floor((256-256)*Math.random()) + 50) + ',' + (Math.floor((256-200)*Math.random()) + 100) + ',' + (Math.floor((256-50)*Math.random()) + 100) + ')';
+    $('head').append('<style>.child{ width:'+squareSize+'px; height: '+squareSize+'px;} .click{background: '+hue+';} .child:hover{background: '+hue+';} .random-coor{background: '+hue+';}</style>');
   $('.child').on({
     click: function() {
+
       var indexCount = $('.wrapper .child');
       var n = indexCount.index(this) + 1;
       var indexPos = $('.wrapper .child:nth-child('+ n +')').position();
-      var positionX = indexPos.left / squareSize;
+      var positionX = (indexPos.left / squareSize) - 1;
       var positionX2 = lineItems - positionX - 1;
       var positionY = (indexPos.top / squareSize) + 1;
       var positionY2 = columnItems - (indexPos.top / squareSize);
@@ -34,12 +45,10 @@ function myFunction() {
       arr.push(select);
       var indexEl = select.position();
       //------------------------left right top level------------------//
-      for (var i = 0; i <= positionX; i++) {
-        (function () {
-          setTimeout(function() {
-            var prevX = n - lrTop;
-            var prev = $('.wrapper .child:nth-child('+ prevX +')');
-            for (var q = 0; q < lrTop; q++) {
+      var interval = setInterval(function () {
+        var prevX = n - lrTop;
+        var prev = $('.wrapper .child:nth-child('+ prevX +')');
+        for (var q = 0; q < lrTop; q++) {
               //---left top--//
               var prevX2 = n - (lineItems * q)  - lrTop;
               var prev2 = $('.wrapper .child:nth-child('+ prevX2 +')');
@@ -48,19 +57,23 @@ function myFunction() {
               var next2 = $('.wrapper .child:nth-child('+ nextX2 +')');
               arr.push(prev2, next2);
             };
-            lrTop++;
-            arr.push(prev);
-          }, i * 500);
-        })(i);
-      };
+            
+            
+            if(lrTop <= positionX){
+              arr.push(prev);
+              lrTop++;
+            }
+            //arr.push(prev);
+            //start();
+
       //------------------------left right bottom level------------------//
-      for (var i = 0; i <= positionX2; i++) {
-        (function () {
-          setTimeout(function() {
-            var nextX = n + lrBottom;
-            var next = $('.wrapper .child:nth-child('+ nextX +')');
-            for (var q = 0; q < lrBottom; q++) {
-              //---right top--//
+
+      var nextX = n + lrBottom;
+      console.log(lrBottom + ' lrBottom');
+      console.log(positionX + ' positionX' )
+      var next = $('.wrapper .child:nth-child('+ nextX +')');
+      for (var q = 0; q < lrBottom; q++) {
+              //---right top--// 
               var prevX3 = n - (lineItems * q)  + lrBottom;
               var prev3 = $('.wrapper .child:nth-child('+ prevX3 +')');
               //---right bottom--//
@@ -68,83 +81,117 @@ function myFunction() {
               var next3 = $('.wrapper .child:nth-child('+ nextX3 +')');
               arr.push(prev3, next3);
             };
-            lrBottom++;
-            arr.push(next);
-          }, i * 500);
-        })(i);
-      };
+            
+            if(lrBottom < (positionX2 - 1)){
+              arr.push(next);
+              lrBottom++;
+            }
+            
+            
+            //start();
       //------------------------top ++ ------------------//
-      for (var i = 0; i <= positionY; i++) {
-        (function () {
-          setTimeout(function() {
-            topY = n - (lineItems * topCount);
-            var top = $('.wrapper .child:nth-child('+ topY +')');
-            arr.push(top);
-            for (var k = 1; k <= topCount; k++) {
-              var topRightY2 = n - (lineItems * topCount) + k;
-              var topRight2 = $('.wrapper .child:nth-child('+ topRightY2 +')');
-              var topLine = topRight2.position();
-              var trLimit = topLine.left / squareSize;
-              var trLimit2 = (indexEl.left / squareSize) + 1;
-              if (trLimit >= trLimit2 && trLimit <= lineItems) {
-                arr.push(topRight2);
-              }
-            };
-            for (var kk = 1; kk <= topCount; kk++) {
-              var topLeftY2 = n - (lineItems * topCount) - kk;
-              var topLeft2 = $('.wrapper .child:nth-child('+ topLeftY2 +')');
-              var topLine2 = topLeft2.position();
-              var tlLimit = topLine2.left / squareSize;
-              var tlLimit2 = (indexEl.left / squareSize) + 1;
-              if (tlLimit <= tlLimit2 ) {
-                arr.push(topLeft2);
-              }
-            };
-            topCount++;
-          }, i * 500);
-})(i);
-};
+
+      topY = n - (lineItems * topCount);
+      var top = $('.wrapper .child:nth-child('+ topY +')');
+      arr.push(top);
+      for (var k = 1; k <= topCount; k++) {
+        var topRightY2 = n - (lineItems * topCount) + k;
+        var topRight2 = $('.wrapper .child:nth-child('+ topRightY2 +')');
+        if(topRight2.length){
+          var topLine =  topRight2.position();
+          var trLimit = topLine.left / squareSize;              
+
+          var trLimit2 = (indexEl.left / squareSize) + 1;
+          if (trLimit >= trLimit2 && trLimit <= lineItems) {
+            arr.push(topRight2);
+          }  
+        }              
+      };
+      for (var kk = 1; kk <= topCount; kk++) {
+        var topLeftY2 = n - (lineItems * topCount) - kk;
+        var topLeft2 = $('.wrapper .child:nth-child('+ topLeftY2 +')');
+        if(topLeft2.length){
+          var topLine2 = topLeft2.position();
+          var tlLimit = topLine2.left / squareSize;
+          var tlLimit2 = (indexEl.left / squareSize) + 1;
+          if (tlLimit <= tlLimit2 ) {
+            arr.push(topLeft2);
+          }  
+        }              
+      };
+      topCount++;
+      start();
+
       //------------------------bottom ------------------//
-      for (var i = 0; i < positionY2; i++) {
-        (function () {
-          setTimeout(function() {
-            bottomY = n + (lineItems * bottomCount);
-            var bottom = $('.wrapper .child:nth-child('+ bottomY +')');
-            arr.push(bottom);
-            for (var kk = 1; kk <= bottomCount; kk++) {
-              var bottomLeftY2 = n + (lineItems * bottomCount) - kk;
-              var bottomLeft2 = $('.wrapper .child:nth-child('+ bottomLeftY2 +')');
-              var bottomLine = bottomLeft2.position();
-              var blLimit = bottomLine.left / squareSize;
-              var blLimit2 = (indexEl.left / squareSize) + 1;
-              if (blLimit <= blLimit2 ) {
-                arr.push(bottomLeft2);
-              }
-            };
-            for (var k = 1; k <= bottomCount; k++) {
-              var bottomRightY2 = n + (lineItems * bottomCount) + k;
-              var bottomRight2 = $('.wrapper .child:nth-child('+ bottomRightY2 +')');
-              var bottomLine2 = bottomRight2.position();
-              var brLimit = bottomLine2.left / squareSize;
-              var brLimit2 = (indexEl.left / squareSize) + 1;
-              if (brLimit >= brLimit2 && brLimit <= lineItems) {
-                arr.push(bottomRight2);
-              }
-            };
-            bottomCount++;
-          }, i * 500);
-          })(i);
-          };
-          for (var i = 1; i < elements; i++) {
-            (function () {
-              setTimeout(function() {
-                jQuery.each(arr, function() {
-                  $(this).addClass('click')
-                })
-              }, i * 500);
-            })(i);
-          };
+
+      bottomY = n + (lineItems * bottomCount);
+      var bottom = $('.wrapper .child:nth-child('+ bottomY +')');
+      arr.push(bottom);
+      for (var kk = 1; kk <= bottomCount; kk++) {
+        var bottomLeftY2 = n + (lineItems * bottomCount) - kk;
+        var bottomLeft2 = $('.wrapper .child:nth-child('+ bottomLeftY2 +')');
+        if(bottomLeft2.length) {
+          var bottomLine = bottomLeft2.position();
+          var blLimit = bottomLine.left / squareSize;
+          var blLimit2 = (indexEl.left / squareSize) + 1;
+          if (blLimit <= blLimit2 ) {
+            arr.push(bottomLeft2);
+          }
         }
-      })
-    }
+
+      };
+      for (var k = 1; k <= bottomCount; k++) {
+        var bottomRightY2 = n + (lineItems * bottomCount) + k;
+        var bottomRight2 = $('.wrapper .child:nth-child('+ bottomRightY2 +')');
+        if(bottomRight2.length) {
+          var bottomLine2 = bottomRight2.position();
+          var brLimit = bottomLine2.left / squareSize;
+          var brLimit2 = (indexEl.left / squareSize) + 1;
+          if (brLimit >= brLimit2 && brLimit <= lineItems) {
+           arr.push(bottomRight2);
+         }
+       }
+
+     };
+     bottomCount++;
+     start();
+
+      if ($('.click').length >= elements){
+        clearInterval(interval);
+        console.log('exit');
+      }
+    },300);
+
+$('.reset').on({
+      click: function() {
+        $('.child').removeClass('click');
+        clearInterval(interval);
+      }
+    })
+}
+
+})
+}
+}
+
+function start () {
+  jQuery.each(arr, function() {
+    $(this).addClass('click');
+  })
+}
+
+function randomColor () {
+  var hue = 'rgb(' + (Math.floor((256-256)*Math.random()) + 50) + ',' + (Math.floor((256-200)*Math.random()) + 100) + ',' + (Math.floor((256-50)*Math.random()) + 100) + ')';
+   $('head').append('<style>.click{background: '+hue+';} .random-coor{background: '+hue+';} .child:hover{background: '+hue+';}</style>');
+}
+
+function setRadius () {
+  $('.child').toggleClass('radius');
+  if($('.child').hasClass('radius')){
+    $('.rad').text('Set Square');
+  }
+  else {
+    $('.rad').text('Set Radius');
+  }
+  
 }
